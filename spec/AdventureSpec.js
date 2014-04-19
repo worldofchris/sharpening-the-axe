c86a2f8b96bc6d8313aa92e3615adf022cd39734
Chris Young <chris@chrisyoung.org>
2014-04-19 22:42:47 +0100

    CYOA-1: Added missing file :-(

--- spec/AdventureSpec.js
+++ spec/AdventureSpec.js
@@ -1,17 +1,32 @@
 describe("Adventure", function() {
 
+var map = {"options":[
+    {
+        "title": "Airports"
+    },
+    {
+        "title": "Fresh Fruit Juice"
+    },
+    {
+        "title": "Art History"
+    }
+]};
+
+  var adventure;
+
+  beforeEach(function() {
+    adventure = new Adventure(map);
+  });
+
   it("should give me some options for my adventure to begin", function() {
 
-    // Given some options
+    // Given some options - resources/options.js
 
     // And a template
-    var options_menu_template = "|{{#options}}{{title}}|{{/options}}";
 
     // I should get this output
-    var template = Handlebars.compile(options_menu_template);
-
     var expected_html = "|Airports|Fresh Fruit Juice|Art History|";
-    expect(template(adventure)).toEqual(expected_html);
+    expect(adventure.root()).toEqual(expected_html);
 
   });
 
